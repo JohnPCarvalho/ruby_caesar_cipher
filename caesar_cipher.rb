c8c8c4c667d17717739f5512be738ce592e71602
JohnPCarvalho <johnnypcarvalho@gmail.com>
2022-01-03 17:06:12 -0300

    Correct solution

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,7 +1,17 @@
-LETTERS = ('a'.ord..'z'.ord)
+def uppercase?(ord)
+  ord.between?(64, 91)
+end
 
-def caesar_cipher(string, n)
-    string.chars.map { |x| LETTERS.include?(x.ord)?((x.ord - LETTERS.min + n) % 26 + LETTERS.min).chr  : x}.join
+def encode(char, shift)
+  ord = char.ord
+  return char unless ord.between?(65, 123)
+  lower_limit, upper_limit = uppercase?(ord) ? [65, 91] : [96, 123]
+  shifted_ord = ord + shift
+  shifted_ord > upper_limit ? (lower_limit + shifted_ord % upper_limit).chr : shifted_ord.chr
+end
+
+def caesar_cipher(string, shift)
+  string.chars.map { |char| encode(char, shift) }.join()
 end
 
 puts caesar_cipher("What a string!", 5)
